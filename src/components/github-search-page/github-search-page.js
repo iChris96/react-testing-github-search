a8590720269da783e9c887d842b7a0a0456a1df3
christopher <christopher_x10x@hotmail.com>
2021-07-26 16:58:55 -0500

    start tdd for github_search proyect

---
+++ src/components/github-search-page/github-search-page.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const GithubSearchPage = () => {
+  const title = 'github repositories list';
+  
+  return (
+    <h1>{title}</h1>
+  )
+}
+
+export default GithubSearchPage
