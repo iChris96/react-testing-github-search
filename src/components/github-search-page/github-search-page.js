04722ba9e554d3d13818dce17a7870b3b36d8c83
christopher <christopher_x10x@hotmail.com>
2021-07-27 16:00:34 -0500

    start to test the page whjen is mounted

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,11 +1,17 @@
-import React from "react"
-import Typography from "@material-ui/core/Typography"
+import React from 'react'
+import {TextField, Typography, Button} from '@material-ui/core'
 
 const GithubSearchPage = () => {
-  const title = 'github repositories list';
-  
+  const title = 'github repositories list'
+
   return (
-    <Typography variant="h4" component="h1" >{title}</Typography>
+    <>
+      <Typography variant="h4" component="h1">
+        {title}
+      </Typography>
+      <TextField label="Filter by" id="filterBy" />
+      <Button>Search</Button>
+    </>
   )
 }
 
--- src/components/github-search-page/github-search-page.test.js
+++ src/components/github-search-page/github-search-page.test.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
-
 import GithubSearchPage from './github-search-page'
 
 describe('when the GithubSearchPage is mounted', () => {
-  it('must display the title', () => {
-    render(<GithubSearchPage />)
+  beforeEach(() => render(<GithubSearchPage />))
 
+  it('must display the title', () => {
     expect(
       screen.getByRole('heading', {name: /github repositories list/i}),
     ).toBeInTheDocument()
+  })
+
+  it('An input text with label "filter by" field in order to do the search', () => {
+    const input = screen.getByLabelText(/filter by/i)
+
+    expect(input).toBeInTheDocument()
+  })
+
+  it('Must be a Seach Button', () => {
+    const button = screen.getByRole('button', {name: /search/i})
+
+    expect(button).toBeInTheDocument()
   })
 })
