4503a79c4fdffcaa32364cd19a8d3187514bbef2
christopher <christopher_x10x@hotmail.com>
2021-07-27 16:08:53 -0500

    styling components

--- src/components/github-search-page/github-search-page.js
+++ src/components/github-search-page/github-search-page.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import {TextField, Typography, Button} from '@material-ui/core'
+import {TextField, Typography, Button, Grid, Container} from '@material-ui/core'
 
 const GithubSearchPage = () => {
   const title = 'github repositories list'
 
   return (
-    <>
+    <Container>
       <Typography variant="h4" component="h1">
         {title}
       </Typography>
-      <TextField label="Filter by" id="filterBy" />
-      <Button>Search</Button>
-    </>
+      <Grid container spacing={2} justify="space-between">
+        <Grid item md={6} xs={12}>
+          <TextField fullWidth label="Filter by" id="filterBy" />
+        </Grid>
+
+        <Grid item md={2} xs={12}>
+          <Button fullWidth color="primary" variant="contained">
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
